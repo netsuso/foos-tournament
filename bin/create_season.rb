9047a131367f468684898ca443595a5efc36f380
Carlos Bello <bellopauste@yahoo.com>
2016-05-18 00:45:11 +0200

    Activates season on creation, if requested

--- bin/create_season.rb
+++ bin/create_season.rb
@@ -3,13 +3,16 @@ $LOAD_PATH << '..'
 require 'season_repository'
 
 if ARGV.length < 1
-  puts "Syntax: create_season.rb <title>"
+  puts "Syntax: create_season.rb <title> [--active|-a]"
   exit
 end
 
 season_title = ARGV[0]
+season_status = (ARGV.length > 1 and ['--active', '-a'].include?(ARGV[1])) ?
+  :playing :
+  :preparing
 
 season_repo = SeasonRepository.new()
 season = Season.new(nil, season_title)
-season.set_status(:preparing, nil, nil)
+season.set_status(season_status, nil, nil)
 season_repo.add(season)
