0a57dc543f650ee705ddc816d4cd52da14bb9f77
Jesús Bravo Álvarez <suso@luar.es>
2016-10-16 22:19:45 +0200

    Fix database path

--- dm/data_model.rb
+++ dm/data_model.rb
@@ -88,7 +88,7 @@ class Match
 end
 
 #DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, 'sqlite:///home/suso/futbolin/pre/foos.db')
+DataMapper.setup(:default, 'sqlite:///home/suso/futbolin/foos.db')
 DataMapper.repository(:default).adapter.resource_naming_convention = DataMapper::NamingConventions::Resource::UnderscoredAndPluralizedWithoutModule
 DataMapper.finalize
 
